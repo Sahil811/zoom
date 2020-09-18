6eb9dd57745e8f5c638ab0254d207ab560c25624
unknown <sahilsiddiqui.dev@gmail.com>
2020-09-18 13:26:44 +0530

    commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const { ExpressPeerServer } = require("peer");
+
+const app = express();
+
+const server = require("http").Server(app);
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+const io = require("socket.io")(server);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use("/peerjs", peerServer);
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+  });
+});
+
+server.listen(8000, () => console.log("Running in port 8000"));
